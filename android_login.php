44b95206c78610936b5e15ad865ba8de46332488
Octo Moon <octahedron01@gmail.com>
2020-12-10 18:35:03 +0900

    2
    2

---
+++ android_login.php
@@ -0,0 +1,11 @@
+<?php
+	$con = mysqli_connect("localhost","crud","qwerty2020","notebamboo");
+	$result = mysqli_query($con, "SELECT pw, nickname FROM user where id='test'");.$_POST["id"]);
+
+	$response = array();
+	while($row = mysqli_fetch_array($result)){
+		$response[] = $row;
+	}
+	echo json_encode(array("response"=>$response));
+	mysqli_close($con);
+ ?>
